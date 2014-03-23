4b9476e9d0012eb8ddbff76d03a2f1339a994783
Jamie Allen <shinolajla@yahoo.com>
2014-03-23 10:26:11 -0700

    Fixed counter incrementing

--- node/sample.js
+++ node/sample.js
@@ -3,7 +3,7 @@ var http = require('http');
 var counter = 0;
 
 http.createServer(function (req, res) {
-  counter += 1;
+  counter++;
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Sending response: ' + counter + ' via callback!\n');
 }).listen(8888, '127.0.0.1');
