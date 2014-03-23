c6f63bccb64fad0b243b3f2840318661fd616777
Jamie Allen <shinolajla@yahoo.com>
2014-03-23 11:52:37 -0700

    Added test for RxJava, but it does not assert anything

--- rxjava/src/main/java/org/reactivedesignpatterns/chapter2/rxjava/RxJavaExample.java
+++ rxjava/src/main/java/org/reactivedesignpatterns/chapter2/rxjava/RxJavaExample.java
@@ -4,20 +4,15 @@ import rx.Observable;
 import rx.functions.Action1;
 
 public class RxJavaExample {
-    public static void observe(String[] strings) {
+    RxJavaExample() {
+    }
+
+    public void observe(String[] strings) {
         Observable.from(strings).subscribe(new Action1<String>() {
             @Override
             public void call(String s) {
                 System.out.println("Received " + s);
             }
         });
-    }
-
-    public static void main(String... args) {
-        if (args.length == 0) {
-            String[] strings = { "a", "b", "c" };
-            observe(strings);
-        } else
-            observe(args);
     }
 }
